4d63da44782395559c76e06c21a4204163828674
callumahern <callum.ahern@simplybusiness.co.uk>
2022-08-16 12:17:17 +0100

    hardcode to make tests pass

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
 
+  def balance
+    0
+  end
+
 end
--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -4,6 +4,7 @@ require 'bank_account'
 
 RSpec.describe 'Bank' do
   it 'expects Person to default with 0 balance in Bank Account' do
+    person = Person.new
     expect(person.balance).to eq 0
   end
 end
