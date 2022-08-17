14a24e0a71f75ed4b55d141cfd7021dc88f8ff5e
callumahern <callum.ahern@simplybusiness.co.uk>
2022-08-17 12:41:24 +0100

    included let statement to keep code dry

--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -3,26 +3,26 @@ require 'customer'
 require 'bank_account'
 
 RSpec.describe 'Bank' do
+  let(:account) { BankAccount.new }
+
   it 'expects Customer to default with 0 balance in Bank Account' do
     @customer = Customer.new
     expect(@customer.account).to eq 0
   end
 
   it 'adding 1 to balance' do
-    @account = BankAccount.new
-    @account.increase_balance(1)
-    expect(@account.balance).to eq 1
+    account = BankAccount.new
+    account.increase_balance(1)
+    expect(account.balance).to eq 1
   end
 
   it 'withdraws 1 from balance' do
-    @account = BankAccount.new
-    @account.increase_balance(1)
-    @account.withdraw_amount(1)
-    expect(@account.balance).to eq 0
+    account.increase_balance(1)
+    account.withdraw_amount(1)
+    expect(account.balance).to eq 0
   end
 
   it 'checks balance' do
-    @account = BankAccount.new
-    expect(@account.check_balance).to eq @account.balance
+    expect(account.check_balance).to eq account.balance
   end
 end
